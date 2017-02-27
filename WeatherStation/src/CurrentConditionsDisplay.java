4d4c3fd31ba126e81afc75565914aead71f6c256
Xavier Perez <Xavier Perez>
2017-02-27 11:24:05 -0800

    Built Display Elements

---
+++ WeatherStation/src/CurrentConditionsDisplay.java
@@ -0,0 +1,31 @@
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement{
+
+	private float temperature;
+	private float humidity;
+	private Subject weatherData;
+	
+	public CurrentConditionsDisplay(Subject weatherData){
+		
+		this.weatherData = weatherData;
+		weatherData.registerObserver(this);
+		
+	}
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("Current conditions: " + temperature
+				+ "F degrees and " + humidity + "% humidity");
+		
+	}
+
+	@Override
+	public void update(float temp, float humidity, float pressure) {
+		// TODO Auto-generated method stub
+		this.temperature = temp;
+		this.humidity = humidity;
+		display();
+		
+	}
+
+}
